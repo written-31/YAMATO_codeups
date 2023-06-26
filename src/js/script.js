77dd6ea37be2ce24718c5bcc0b8cc3f95d3ae1a5
written-31 <115435294+written-31@users.noreply.github.com>
2023-06-26 21:13:46 +0900

    修正1回目

--- src/js/script.js
+++ src/js/script.js
@@ -1,70 +1,158 @@
-
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
 
   // 呼び出しの基本：
-  const swiper = new Swiper('#slide1', {
+  const swiper = new Swiper("#slide1", {
     // ここにオプション設定が入ります
-    direction: 'horizontal',
+    direction: "horizontal",
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 4000,
       disableOnInteraction: false,
     },
-    effect: 'fade',
+    effect: "fade",
     fadeEffect: {
       crossFade: true,
     },
   });
 
   // campaignスライダー
-  const swiper2 = new Swiper('#slide2', {
-
+  const swiper2 = new Swiper("#slide2", {
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 4000,
       disableOnInteraction: false,
     },
 
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
 
     breakpoints: {
       // when window width is >= 320px
       320: {
         slidesPerView: 1.31,
-        spaceBetween: 24
+        spaceBetween: 24,
       },
       // when window width is >= 640px
       768: {
         slidesPerView: 3.5,
-        spaceBetween: 24
+        spaceBetween: 24,
       },
       1440: {
         slidesPerView: 4,
-        spaceBetween: 40
+        spaceBetween: 40,
       },
-
-    }
+    },
   });
 
   //ハンバーガーメニュー
-  $('.js-hamburer').on('click', function() {
-    $(this).toggleClass('open');
-    if($(this).hasClass('open')) {
-      $('.js-nav').fadeIn();
-      $('.header').addClass('transparent');
-      $('body').css('overflow', 'hidden'); // スクロール無効化
-      } else {
-        $('.js-nav').fadeOut();
-        $('.header').removeClass('transparent');
-        $('body').css('overflow', 'auto'); // スクロール有効化
+  $(".js-hamburer").on("click", function () {
+    $(this).toggleClass("open");
+    if ($(this).hasClass("open")) {
+      $(".js-nav").fadeIn();
+      $(".header").addClass("transparent");
+      $("body").css("overflow", "hidden"); // スクロール無効化
+    } else {
+      $(".js-nav").fadeOut();
+      $(".header").removeClass("transparent");
+      $("body").css("overflow", "auto"); // スクロール有効化
+    }
+  });
+
+  //要素の取得とスピードの設定
+  var box = $(".colorbox"),
+    speed = 500;
+
+  //.colorboxの付いた全ての要素に対して下記の処理を行う
+  box.each(function () {
+    $(this).append('<div class="color"></div>');
+    var color = $(this).find($(".color")),
+      image = $(this).find("img");
+    var counter = 0;
+
+    image.css("opacity", "0");
+    color.css("width", "0%");
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on("inview", function () {
+      if (counter == 0) {
+        $(this)
+          .delay(200)
+          .animate({ width: "100%" }, speed, function () {
+            image.css("opacity", "1");
+            $(this).css({ left: "0", right: "auto" });
+            $(this).animate({ width: "0%" }, speed);
+          });
+        counter = 1;
       }
     });
+  });
+
+
+    
+  // ページトップボタン
+  var topBtn = $('#js-pagetop');
+  topBtn.hide();
+
+  // ページトップボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ページトップボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300, 'swing');
+    return false;
+  });
+
+
+  $(document).ready(function() {
+    var pagetop = $('#js-pagetop');
+    var svgPath = pagetop.find('svg path');
+    var footer = $('footer');
+    var footerHeight = footer.outerHeight();
+  
+    $(window).scroll(function() {
+      var scrollTop = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      var scrollBottom = scrollTop + windowHeight;
+  
+      if (scrollBottom >= footer.offset().top) {
+        pagetop.css('border-color', '#fff');
+        svgPath.css('stroke', '#fff'); // SVGパスの色を変更
+      } else {
+        pagetop.css('border-color', '');
+        svgPath.css('stroke', ''); // デフォルトの色に戻す
+      }
+    });
+  });
+  
 
 
 
-
+  function matchHeight(elements) {
+    const target = Array.from(document.querySelectorAll(elements));
+    const heightList = [];
+    target.forEach(element => {
+     const height = element.clientHeight;
+     heightList.push(height);
+    });
+    const maxHeight = Math.max.apply(null,heightList);
+    target.forEach(element => {
+     element.style.height = maxHeight + 'px'; // 最大高さに揃える
+    });
+   }
+   
+   matchHeight('.campaign-card__body');
 
 });
+
