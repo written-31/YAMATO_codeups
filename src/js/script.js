6770cb26a26f0c77e369593e4431c1752c07c87f
written-31 <115435294+written-31@users.noreply.github.com>
2023-07-05 01:36:17 +0900

    npmバージョン更新

--- src/js/script.js
+++ src/js/script.js
@@ -143,20 +143,20 @@ jQuery(function ($) {
 
 
 
-  function matchHeight(elements) {
-    const target = Array.from(document.querySelectorAll(elements));
-    const heightList = [];
-    target.forEach(element => {
-     const height = element.clientHeight;
-     heightList.push(height);
-    });
-    const maxHeight = Math.max.apply(null,heightList);
-    target.forEach(element => {
-     element.style.height = maxHeight + 'px'; // 最大高さに揃える
-    });
-   }
+  // function matchHeight(elements) {
+  //   const target = Array.from(document.querySelectorAll(elements));
+  //   const heightList = [];
+  //   target.forEach(element => {
+  //    const height = element.clientHeight;
+  //    heightList.push(height);
+  //   });
+  //   const maxHeight = Math.max.apply(null,heightList);
+  //   target.forEach(element => {
+  //    element.style.height = maxHeight + 'px'; // 最大高さに揃える
+  //   });
+  //  }
    
-   matchHeight('.campaign-card__body');
+  //  matchHeight('.campaign-card__body');
 
 });
 
